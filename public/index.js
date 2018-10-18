475f8ed231d8957715785b621134b91b9ffb56f0
Adam Gonzales <adamgonzales1@gmail.com>
2018-10-17 23:57:56 -0700

    start writing event listeners and conditionals

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
-const header = document.querySelector('#header');
-const headerContainer = document.querySelector('.page-container');
+const calculatorContainer = document.querySelector(".calculator-container");
+const keysContainer = calculatorContainer.querySelector(".keys-container");
 
-const salesRepContainer = document.getElementById('salesRep');
-
-const titles = ["energy consultant", "technical sales representative", "solar consultant", "solar sales specialist", "solar energy consultant", "account manager", "solar sales engineer"];
-
-const randomTitle = titles[Math.floor(Math.random() * titles.length)];
-
-const requestButton = document.getElementById("requestRep");
-requestButton.addEventListener("click", requestData);
-// requestData();
+keysContainer.addEventListener("click", e => {
+  if (e.target.matches(".button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    if (!action) {
+      console.log('number key');
+    }
+  }
+});
